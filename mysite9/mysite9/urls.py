9a0ad82cf9dd8fc8cc2e2e53c86a154c3084a502
songhyeonbeom <erekude@naver.com>
2022-02-16 12:03:32 +0900

    학원입니다. 0216

--- mysite9/mysite9/urls.py
+++ mysite9/mysite9/urls.py
@@ -14,11 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from shop import views
 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('shop/', include('shop.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns +- static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns +- static(settings.MEDIA_URL, document_root=settings.STATIC_ROOT)
+
 
 
