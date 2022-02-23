ffc326537ee28a87ed4a3c0f04b881d0089ef8e6
songhyeonbeom <erekude@naver.com>
2022-02-23 10:40:12 +0900

    이동

--- TensorFlow2/YS/DeepLearningZeroToAll/lab-07-4-mnist_introduction.py
+++ TensorFlow2/YS/DeepLearningZeroToAll/lab-07-4-mnist_introduction.py
@@ -10,7 +10,9 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 # Check out https://www.tensorflow.org/get_started/mnist/beginners for
 # more information about the mnist dataset
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+# mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+mnist = tf.keras.datasets.mnist
 
 nb_classes = 10
 
