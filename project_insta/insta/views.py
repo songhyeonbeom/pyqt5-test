5649a6a45c95333d2eb7c9f3a74a49b167bbc6f3
songhyeonbeom <erekude@naver.com>
2022-03-21 00:12:19 +0900

    업데이트한다.

--- project_insta/insta/views.py
+++ project_insta/insta/views.py
@@ -1,7 +1,4 @@
-from django.shortcuts import render
-
 from django.views.generic import ListView, DetailView
-from insta.models import Album, Photo
 from django.views.generic import CreateView, UpdateView, DeleteView
 
 from django.shortcuts import redirect
