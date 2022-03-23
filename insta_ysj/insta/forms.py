862d7f8bc05002c200bf42068374dede085b8439
songhyeonbeom <erekude@naver.com>
2022-03-23 16:52:58 +0900

    ggg

--- insta_ysj/insta/forms.py
+++
@@ -1,6 +0,0 @@
-from django.forms import inlineformset_factory
-from insta.models import Album, Photo
-
-PhotoInlineFormSet = inlineformset_factory(Album, Photo,
-                                           fields=['image', 'title', 'description'],
-                                           extra=2)
