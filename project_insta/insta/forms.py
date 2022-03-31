2ca6f79f865b95f4031f69947b55540af9b8f8d2
songhyeonbeom <erekude@naver.com>
2022-03-31 19:33:04 +0900

    완료했는가.

--- project_insta/insta/forms.py
+++ project_insta/insta/forms.py
@@ -3,28 +3,20 @@ from insta.models import Album, Photo, Answer
 from django import forms
 
 PhotoInlineFormSet = inlineformset_factory(Album, Photo,
-                                           fields=['image', 'title', 'description',],
+                                           fields=['image', 'description',],
                                            extra=2)
-
 
 class PhotoForm(forms.ModelForm):
     class Meta:
         model = Photo  # 사용할 모델
-        fields = ['title', 'description']  # QuestionForm에서 사용할 Question 모델의 속성
+        fields = ['description']
         # 'name',
         widgets = {
-
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
             'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 10}),
-
         }
         labels = {
-
-            'title': '제목',
             'description': '내용',
         }
-
-
 
 class AnswerForm(forms.ModelForm):
     class Meta:
